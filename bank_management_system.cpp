d27fa63ab293d0dc28e0d197c4306842f2da7857
Yogeash <87556459+Yogeash001@users.noreply.github.com>
2021-10-18 08:51:48 +0530

    Added File

--- bank_management_system.cpp
+++ bank_management_system.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 
-class Bank_management_sys {
+class Bank_management_sys 
+
+{
 
 private:
 
@@ -13,13 +15,20 @@ private:
 
 public:
 
+    Bank_management_sys(){
+        acno = 0 ;
+        balance = 0  ;
+    }
+
     void Open_Account();
     void Show_Account();
-    void Deposit();
+    void deposit();
     void Withdrawal();
     int Search_id(int);
 
 };
+
+
 
 void Bank_management_sys :: Open_Account(){
         
@@ -34,15 +43,20 @@ void Bank_management_sys :: Open_Account(){
 
 void Bank_management_sys :: Show_Account(){
 
-        cout << "Account Number: " << acno << endl;
-        cout << "Name: " << name << endl;
-        cout << "Balance: " << balance << endl;
+        cout<<"-----------------------------------\n";
+        cout<<"\n";
+        cout << "\tAccount Number: " << acno << endl;
+        cout << "\tName: " << name << endl;
+        cout << "\tBalance Ammount: " << balance << endl;
+        cout<<"\n";
+        cout<<"-----------------------------------";
         cout<<endl;
 }
 
-void Bank_management_sys :: Deposit(){
+void Bank_management_sys :: deposit(){
         
         long amt;
+        cout<<endl;
         cout << "Enter Amount U want to deposit? ";
         cin >> amt;
         balance = balance + amt;
@@ -54,6 +68,7 @@ void Bank_management_sys :: Withdrawal()
         long amt;
         cout << "Enter Amount U want to withdraw? ";
         cin >> amt;
+        cout<<endl;
         if (amt <= balance){
 
             balance = balance - amt;
@@ -77,15 +92,17 @@ int Bank_management_sys::Search_id(int a)
 // main Program
 int main()
 {
-    // Variable Decleration 
+
+    cout<<"\n\t\t\t BANK MANAGEMENT SYSTEM \n\n\n";
     
+    // Variable Decleration 
     int n ;
-    int found = 0; 
+    int to_be_found = 0; 
     int a; 
     int ch;
 
-    // To get number of datas to be entered
-
+    // To get number of data's to be entered
+    
     cout << "Enter the No of details to be Entered : ";
     cin>>n ; 
     cout<<endl;
@@ -94,18 +111,17 @@ int main()
    
     Bank_management_sys C[n]; 
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
         C[i].Open_Account();
-    }
-
+    
     do {
         
         // Display options
         
-        cout << "\n1:Display All Accounts \n2: Display By Account No\n3: Deposit\n4: Withdraw\n5: Exit" << endl;
+        cout << "\n\t1: Display All Accounts \n\t2: Display By Account No\n\t3: Deposit\n\t4: Withdraw\n\t5: Exit" << endl;
 
         // User input
-        
+        cout<<'\n';
         cout << "Please enter your choice: ";
         cin >> ch;
         cout<<endl;
@@ -122,60 +138,64 @@ int main()
     
             cout << "Account Number: ";
             cin >> a;
+            cout<<endl;
 
             for (int i = 0; i < n; i++) {
-                found = C[i].Search_id(a);
-                if (found)
+                to_be_found = C[i].Search_id(a);
+                if (to_be_found)
                     break;
             }
 
-            if (!found)
-                cout << "Record Not Found" << endl;
+            if (!to_be_found)
+                cout << "Record Not Found !" << endl;
             break;
        
        case 3: // deposit operation
             
-            cout << "Account Number To Deposit Amount? ";
+            cout << "Enter Account Number To Deposit Amount: ";
             cin >> a;
+            cout<<endl;
             
             for (int i = 0; i < n; i++) {
-                found = C[i].Search_id(a);
-                if (found) {
-                    C[i].Deposit();
+                to_be_found = C[i].Search_id(a);
+                if (to_be_found) {
+                    C[i].deposit();
+                    cout<<"   Account Detail After Deposit ! \n";
+                    C[i].Show_Account();
                     break;
                 }
             }
             
-            if (!found)
-                cout << "Record Not Found" << endl;
+            if (!to_be_found)
+                cout << "Record Not Found !" << endl;
             break;
         
         case 4: // withdraw operation
             
-            cout << "Account Number To Withdraw Amount? ";
+            cout << "Enter Account Number To Withdraw Amount: ";
             cin >> a;
             for (int i = 0; i < n; i++) {
-                found = C[i].Search_id(a);
-                if (found) {
+                to_be_found = C[i].Search_id(a);
+                if (to_be_found) {
                     C[i].Withdrawal();
                     break;
                 }
             }
             
-            if (!found)
-                cout << "Record Not Found" << endl;
+            if (!to_be_found)
+                cout << "Record Not Found !" << endl;
             break;
         
         case 5: // exit
             
-            cout << "Have a nice day" << endl;
+            cout << "Thank You ! For using Our System \n" << endl;
             break;
         
         default:
             
-            cout << "Wrong Option" << endl;
+            cout << " Incorrect  Option" << endl;
         }
     } while (ch != 5);
-    
+
     return 0;
 }
